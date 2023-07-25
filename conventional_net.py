a17a4629af357e24876614894f420c42227c8ca3
Manuelstv <manuel.stveras@gmail.com>
2023-07-25 19:43:48 -0300

    running traind and test

--- conventional_net.py
+++ conventional_net.py
@@ -8,8 +8,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch.nn as nn
 from sklearn.metrics import accuracy_score, precision_score, recall_score
-
 import torch.nn.functional as F
+torch.cuda.empty_cache()
+from sklearn.metrics import hamming_loss, jaccard_score, f1_score
 
 class CustomDataset(Dataset):
     def __init__(self, dataframe, directory, transform=None):
@@ -25,8 +26,11 @@ class CustomDataset(Dataset):
         image = Image.open(img_name).convert('RGB')
         labels = torch.tensor(self.dataframe.iloc[idx, 2:].values.astype(np.float32))  # Convert labels to float32
 
+
         if self.transform:
             image = self.transform(image)
+        #print(image)
+
 
         return image, labels
 
@@ -47,16 +51,16 @@ train_dataset = CustomDataset(train_df, "/home/mstveras/structured3d_repo/struct
 test_dataset = CustomDataset(test_df, "/home/mstveras/structured3d_repo/struct3d_images", transform=data_transform)
 
 # Define batch size and set shuffle to True (if needed)
-batch_size = 2
+batch_size = 3
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 
 
-class CustomCNN(nn.Module):
+class CustomCNN2(nn.Module):
     def __init__(self):
-        super(CustomCNN, self).__init__()
+        super(CustomCNN2, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
@@ -88,15 +92,16 @@ class CustomCNN(nn.Module):
         x = self.dropout(x)
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
+        #x = nn.Sigmoid(x)
 
         return x
 
 
 
 # Define the model
-class CustomCNN2(nn.Module):
+class CustomCNN(nn.Module):
     def __init__(self):
-        super(CustomCNN2, self).__init__()
+        super(CustomCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 3*2, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(3*2, 3*4, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(3*4, 3*8, kernel_size=3, padding=1)
@@ -108,7 +113,7 @@ class CustomCNN2(nn.Module):
         self.conv9 = nn.Conv2d(3*256, 3*512, kernel_size=3, padding=1)
         #self.conv6 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
         #self.fc = nn.Linear(8192, 4096)
-        self.fully = nn.Sequential(nn.Linear(6144, 3072), nn.Dropout(0.5), nn.Linear(3072, 40))
+        self.fully = nn.Sequential(nn.Linear(3072, 1536), nn.Dropout(0.2), nn.Linear(1536, 40))
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
@@ -118,11 +123,11 @@ class CustomCNN2(nn.Module):
         x = F.relu(F.max_pool2d(self.conv5(x), 2)) 
         x = F.relu(F.max_pool2d(self.conv6(x), 2))
         x = F.relu(F.max_pool2d(self.conv7(x), 2))
-        x = F.relu(F.max_pool2d(self.conv8(x), 2)) 
+        #x = F.relu(F.max_pool2d(self.conv8(x), 2)) 
         #x = F.relu(F.max_pool2d(self.conv9(x), 2))
         
         #print(x.size())
-        x = x.view(-1, 6144) 
+        x = x.view(-1, 3072) 
         x = self.fully(x)
         #print(x.size())
         return x
@@ -137,11 +142,11 @@ if torch.cuda.is_available():
     model = model.cuda()
 
 # Define the optimizer and loss function
-optimizer = torch.optim.RMSprop(model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-1)
 criterion = nn.BCEWithLogitsLoss()
 
 # Training parameters
-epochs = 10
+epochs = 30
 
 # Training loop
 for epoch in range(epochs):
@@ -161,6 +166,8 @@ for epoch in range(epochs):
 
         # Forward pass
         outputs = model(inputs)
+        #outputs = torch.sigmoid(outputs)
+        #print(torch.sigmoid(outputs))
 
         # Calculate loss
         loss = criterion(outputs, labels)
@@ -176,14 +183,17 @@ for epoch in range(epochs):
     # Print training statistics for each epoch
     print(f"Epoch [{epoch + 1}/{epochs}], Loss: {running_loss / total_samples:.4f}")
 
-model.load_state_dict(torch.load("trained_model_weights.pth"))
-#torch.save(model.state_dict(), "trained_model_weights.pth")
+
+torch.save(model.state_dict(), "trained_model_weights.pth")
+
+#model.load_state_dict(torch.load("trained_model_weights.pth"))
 
 def evaluate_model(model, data_loader):
     model.eval()  # Set the model to evaluation mode
     device = next(model.parameters()).device
     test_loss = 0
     test_total_samples = 0
+    correct_predictions = 0
 
     with torch.no_grad():
         all_predictions = []
@@ -198,28 +208,31 @@ def evaluate_model(model, data_loader):
             #print(outputs)
 
             # Convert the sigmoid outputs to binary predictions
-            predictions = (outputs).float()
+            predictions = (torch.sigmoid(outputs)> 0.5).float()
 
             all_predictions.append(predictions.cpu())
             all_labels.append(labels.cpu())
-            print(all_predictions)
+            #print(all_predictions)
+            correct_predictions += torch.sum(predictions == labels).item()
 
             # Calculate test loss
             test_loss += criterion(outputs, labels).item()
             test_total_samples += inputs.size(0)
 
         all_predictions = torch.cat(all_predictions)
-        all_labels = torch.cat(all_labels)  
+        all_labels = torch.cat(all_labels)
+        accuracy = correct_predictions / test_total_samples  
 
-    accuracy = accuracy_score(all_labels, all_predictions)
-    precision = precision_score(all_labels, all_predictions, average='micro')
-    recall = recall_score(all_labels, all_predictions, average='micro')
+        #jaccard_index = jaccard_score(all_labels, all_predictions, average='micro')
+    #accuracy = accuracy_score(all_labels, all_predictions)
+    #%precision = precision_score(all_labels, all_predictions, average='micro')
+    #recall = recall_score(all_labels, all_predictions, average='micro')
 
-    return accuracy, precision, recall
+    return accuracy
 
 # Assuming you have already trained the model and have the test_loader
-accuracy, precision, recall = evaluate_model(model, test_loader)
+accuracy = evaluate_model(model, test_loader)
 
 print(f"Test Accuracy: {accuracy:.4f}")
-print(f"Test Precision: {precision:.4f}")
-print(f"Test Recall: {recall:.4f}")
+#print(f"Test Precision: {precision:.4f}")
+#print(f"Test Recall: {recall:.4f}")
